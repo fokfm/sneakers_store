e0a329f1f12d47f139bdd6950fc3f479a04b43fb
fokfm <fokovmaksim@mail.ru>
2024-02-19 13:23:54 +0300

    Add drawer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import Card from "./components/Card";
+import React, {useEffect, useState} from "react";
+import Card from "./components/Card/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+
+
 function App() {
+
+  const urlItems = 'https://65d24eef987977636bfc3b74.mockapi.io/sneackers_api/v1/items';
+
+  const [items, setItems] = useState([]);
+  const [cardItems, setCartItems] = useState([]);
+  const [cartOpened, setCartOpened] = useState(false);
+
+  useEffect(() =>{
+    fetch(urlItems)
+        .then(response => response.json())
+        .then(json => setItems(json));
+
+  },[])
+
+  const onAddToCard = (obj) => {
+    setCartItems((prev) => [...prev, obj]);
+  }
+  console.log(cardItems)
+
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
-      <div className="content p-45">
+      {cartOpened && <Drawer items={cardItems} onClickCart ={() => setCartOpened(!cartOpened)}/>}
+      <Header onClickCart ={() => setCartOpened(!cartOpened)}/>
+      <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>All sneakers</h1>
           <div className="search-block d-flex">
@@ -15,8 +38,16 @@ function App() {
             <input placeholder="Search..." />
           </div>
         </div>
-        <div className="sneakers d-flex">
-          <Card />
+        <div className="sneakers d-flex flex-wrap"  >
+          {
+            items.map(item => <Card
+                title ={item.title}
+                price ={item.price}
+                imageUrl ={item.imageUrl}
+                onFavorite = {() => console.log('Добавили в закладки')}
+                onPlus = {(obj) => onAddToCard(obj)}
+            />)
+          }
         </div>
       </div>
     </div>
---
+++ src/components/Card/Card.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from './Card.module.scss'
+
+function Card({title, imageUrl, price, onFavorite, onPlus}) {
+
+    const [isAdded, setIsAdded] = React.useState(false);
+
+    const onClickPlus = () => {
+        onPlus({title, imageUrl, price});
+        setIsAdded(!isAdded)
+    }
+
+
+    // React.useEffect(() =>{
+    //   console.log('useeffect work');
+    // }, [isAdded]);
+
+
+    return (
+
+        <div className={styles.card}>
+            <div className={styles.favorite}>
+                <img src="/img/heard-unliked.svg" alt="no img"/>
+            </div>
+            <img src={imageUrl} alt="no img" width={133} height={112}/>
+            <h5>{title}</h5>
+            <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column ">
+                    <span>Price:</span>
+                    <b>{price + ' $'}</b>
+                </div>
+
+                <img className={styles.plus} onClick={onClickPlus}
+                     src={isAdded ? '/img/button-checked.svg' : '/img/button-unchecked.svg'} alt="no img"/>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default Card;
